e8f3183a3da0206bd45ef6a5925120d8b0cc4f3b
Ethan Zammit <ethanzammit@gmail.com>
2022-01-08 17:05:50 +0100

    Investigating coords

---
+++ testing_labels.py
@@ -0,0 +1,14 @@
+Day_1 = "Gozo Ferry/footage2"
+Night_1 = "Gozo Ferry/nightfootage2"
+Rain_1 = "Gozo Ferry/rainTest"
+Day_2 = "Gozo Ferry/footage3"
+Night_2 = "Gozo Ferry/20190127_200452"
+
+from main import *
+j
+vid_path = 'videoshjkjjhjjjjjhhjhjjjjjjj
+j
+print('Found {} videos'.format(len(videos)))
+jjjjhjj
+example_path = videos[-7]
+label_videos([example_path], True, 10)
